72a6c5607842a4a515f6a40a23093b261b029b28
Karpfen <karpfen@karpfen.dev>
2024-09-23 22:23:34 +0200

    update: amber
    fix wrong index in args
    
    use exit builtin

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,5 +1,5 @@
-#!/nix/store/vpvy79k1qq02p1vyqjk6nb89gwhxqvyb-bash-5.2p32/bin/bash
+#!/nix/store/izpf49b74i15pcr9708s3xdwyqs4jxwl-bash-5.2p32/bin/bash
 # Written in [Amber](https://amber-lang.com/)
 # version: 0.3.5-alpha
-# date: 2024-09-10 12:46:17
+# date: 2024-09-23 20:23:36
 echo "Hello world"'!'""
